f905a8ef817aa729ce0d21b5ac4ded1ae270a16f
summerbird <8296697+zhang-junzhao@user.noreply.gitee.com>
2023-11-12 16:22:51 +0800

    修复数据库初始化

--- sql/ssadmin_init.sql
+++ sql/ssadmin_init.sql
@@ -21,8 +21,8 @@ SET time_zone = "+00:00";
 -- 数据库： `ssadmin`
 --
 -- 创建新数据库 ssadmin 并选择
-CREATE DATABASE IF NOT EXISTS `ssadmin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `ssadmin`;
+CREATE DATABASE IF NOT EXISTS `rbac` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `rbac`;
 
 -- --------------------------------------------------------
 
@@ -30,29 +30,22 @@ USE `ssadmin`;
 -- 表的结构 `system_users`
 --
 
-CREATE TABLE `system_users` (
-  `id` bigint NOT NULL,
+CREATE TABLE `sys_user` (
+  `userId` bigint NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `nickname` varchar(255) DEFAULT NULL,
-  `status` int NOT NULL DEFAULT '0',
-  `login_ip` varchar(255) DEFAULT NULL,
-  `login_date` datetime DEFAULT NULL,
-  `tenant_id` int DEFAULT '1',
-  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
-  `update_time` datetime DEFAULT CURRENT_TIMESTAMP,
-  `creator` varchar(255) DEFAULT NULL,
-  `updater` varchar(255) DEFAULT NULL,
-  `deleted` tinyint(1) DEFAULT '0'
+ 
+  `status` varchar(255) NOT NULL,
+  `phone`  varchar(255) NOT NULL,
+  `email`  varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- 转存表中的数据 `system_users`
 --
 
-INSERT INTO `system_users` (`id`, `username`, `password`, `nickname`, `status`, `login_ip`, `login_date`, `tenant_id`, `create_time`, `update_time`, `creator`, `updater`, `deleted`) VALUES
-(1, 'sysadmin', '$2a$10$2n91ncs5vNaKEwcuk9dFVuwpm8o3kpj17YW0Ne.6.e.AF.m0MuBPa', 'admin', 0, NULL, '2023-10-28 10:57:02', 1, '2023-10-28 10:57:30', '2023-10-28 10:57:30', NULL, NULL, 0),
-(2, 'user', '$2a$10$2n91ncs5vNaKEwcuk9dFVuwpm8o3kpj17YW0Ne.6.e.AF.m0MuBPa', 'admin', 0, NULL, '2023-10-28 10:57:02', 1, '2023-10-28 10:57:30', '2023-10-28 10:57:30', NULL, NULL, 0);
+INSERT INTO `sys_user` (`userId`, `username`, `password`,  `status`, `phone`,`email`) VALUES
+(1, 'zjz', '1234', '0','13031939607','4242@qq.com')
 
 --
 -- 转储表的索引
@@ -61,9 +54,8 @@ INSERT INTO `system_users` (`id`, `username`, `password`, `nickname`, `status`, 
 --
 -- 表的索引 `system_users`
 --
-ALTER TABLE `system_users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+ALTER TABLE `sys_user`
+  ADD PRIMARY KEY (`userId`),
 
 --
 -- 在导出的表使用AUTO_INCREMENT
@@ -72,8 +64,8 @@ ALTER TABLE `system_users`
 --
 -- 使用表AUTO_INCREMENT `system_users`
 --
-ALTER TABLE `system_users`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `sys_user`
+  MODIFY `userId` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
